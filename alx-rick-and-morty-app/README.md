2888248e05af11d73faa4d67761b161a8a747726
manuel4life <allotey4eva@gmail.com>
2025-08-22 21:55:47 +0200

    files

---
+++ alx-rick-and-morty-app/README.md
@@ -0,0 +1,10 @@
+# ALX Rick and Morty App Setup
+
+## Project Initialization
+
+1. Create and navigate to your project directory:
+
+```bash
+mkdir -p alx-graphql-0x01/alx-rick-and-morty-app
+cd alx-graphql-0x01/alx-rick-and-morty-app
+```
---
+++ alx-rick-and-morty-app/components/ErrorBoundary.tsx
@@ -0,0 +1,40 @@
+import React from "react";
+import { ReactNode } from "react";
+import * as Sentry from "@sentry/react";
+import { ErrorBoundaryProps, State } from "../interfaces";
+
+class ErrorBoundary extends React.Component<ErrorBoundaryProps, State> {
+  constructor(props: ErrorBoundaryProps) {
+    super(props);
+    this.state = {
+      hasError: false,
+    };
+  }
+
+  static getDerivedStateFromError(error: Error): State {
+    return { hasError: true };
+  }
+
+  componentDidCatch(error: Error, errorInfo: React.ErrorInfo) {
+    Sentry.captureException(error, {
+      extra: { componentStack: errorInfo.componentStack },
+    });
+  }
+
+  render() {
+    if (this.state.hasError) {
+      return (
+        <div>
+          <h2>Oops, there is an error!</h2>
+          <button onClick={() => this.setState({ hasError: false })}>
+            Try again?
+          </button>
+        </div>
+      );
+    }
+
+    return this.props.children;
+  }
+}
+
+export default ErrorBoundary;
